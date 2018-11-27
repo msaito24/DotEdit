5e472efcf5841bf9017164f26d82f578295ad4b3
Kohei Imatomi <kohei.imatomi@gmail.com>
2018-11-27 19:03:29 +0900

    ExportButton 実装

--- Assets/DotEdit/Scripts/ExportButtonUI.cs
+++ Assets/DotEdit/Scripts/ExportButtonUI.cs
@@ -7,10 +7,24 @@ public class ExportButtonUI : MonoBehaviour
     [Header("外部参照")]
     [SerializeField] DotCanvasUI dotCanvasUI;
 
+    private MakeTexture _make;
+    public MakeTexture make
+    {
+        get
+        {
+            if(_make == null)
+            {
+                _make = GetComponent<MakeTexture>();
+            }
+            return _make;
+        }
+    }
+
     public void OnSelected()
     {
         Color[] colors = dotCanvasUI.colors;
 
         // Save colors as png
+        make.SavePng(colors);
     }
 }
