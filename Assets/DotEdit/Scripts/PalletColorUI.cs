921e536492b36e55e2605436035a5c31b6befa57
Kohei Imatomi <kohei.imatomi@gmail.com>
2018-11-27 18:58:56 +0900

    パレット編集機能実装

--- Assets/DotEdit/Scripts/PalletColorUI.cs
+++ Assets/DotEdit/Scripts/PalletColorUI.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // パレットの色
 public class PalletColorUI : MonoBehaviour
@@ -9,6 +10,21 @@ public class PalletColorUI : MonoBehaviour
     public Color color;
 
     public Action<Color> onSelected;
+
+	private Image _image;
+	public Image image {
+		get {
+			if (_image == null) {
+				_image = GetComponent<Image>();
+			}
+			return _image;
+		}
+	}
+
+	public void SetColor(Color _color) {
+		this.color = _color;
+		this.image.color = _color;
+	}
 
     public void OnSelected()
     {
