81524493e36edfe71a52e2f7ff33d5a64a5401d1
Kohei Imatomi <kohei.imatomi@gmail.com>
2018-11-27 18:13:10 +0900

    各種UI用のスクリプトを追加

---
+++ Assets/DotEdit/Scripts/DotCanvasUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotCanvasUI : MonoBehaviour {
+	public int sizeX;
+	public int sizeY;
+	public Color[] colors;
+
+	// キャンバスの一部がクリックされたときに呼び出される
+	public void OnClicked(Vector2 position) {
+
+	}
+}
