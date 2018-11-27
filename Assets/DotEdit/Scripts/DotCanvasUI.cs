53fdbf2795ed6f75cda2bd3f63a9a3719d645e78
Kohei Imatomi <kohei.imatomi@gmail.com>
2018-11-27 19:50:03 +0900

    透明設定

--- Assets/DotEdit/Scripts/DotCanvasUI.cs
+++ Assets/DotEdit/Scripts/DotCanvasUI.cs
@@ -9,6 +9,10 @@ public class DotCanvasUI : MonoBehaviour
     public DotCellUI cellTemplate;
     public Transform cellParent;
 
+    [Header("設定")]
+    public Color alpha1;
+    public Color alpha2;
+
     [Header("デバッグ")]
     public DotCellUI[] cells;
     public Color[] colors;
@@ -31,12 +35,13 @@ public class DotCanvasUI : MonoBehaviour
             for(int j = 0; j < sizeY; j++)
             {
                 Point point = new Point(i,j);
-                int index = DotEditUtils.GetIndexFromPoint(point, sizeX);
-                DotCellUI cell = Instantiate<DotCellUI>(cellTemplate, cellParent);
+                int index = DotEditUtils.GetIndexFromPoint(point,sizeX);
+                DotCellUI cell = Instantiate<DotCellUI>(cellTemplate,cellParent);
                 cell.point = point;
                 cell.onDraw = DrawDot;
                 cell.gameObject.SetActive(true);
                 cells[index] = cell;
+                Erase(point);
             }
         }
 
@@ -57,14 +62,14 @@ public class DotCanvasUI : MonoBehaviour
 
         if(0 <= index && index < cells.Length)
         {
-            if (currentToolType == ToolType.Pencil) {
+            if(currentToolType == ToolType.Pencil)
+            {
                 cells[index].image.color = color;
                 colors[index] = color;
             }
-            else if (currentToolType == ToolType.Eraser) {
-                color = new Color(0f, 0f, 0f, 0f);
-                cells[index].image.color = color;
-                colors[index] = color;
+            else if(currentToolType == ToolType.Eraser)
+            {
+                Erase(point);
             }
         }
         else
@@ -73,7 +78,24 @@ public class DotCanvasUI : MonoBehaviour
         }
     }
 
-    public void ChangeCurrentTool(ToolType type) {
+    public void Erase(Point point)
+    {
+        int d = point.y % 2;
+        int index = DotEditUtils.GetIndexFromPoint(point,sizeX);
+
+        if((point.x + d) % 2 == 0)
+        {
+            cells[index].image.color = alpha1;
+        }
+        else
+        {
+            cells[index].image.color = alpha2;
+        }
+        colors[index] = new Color(0f,0f,0f,0f);
+    }
+
+    public void ChangeCurrentTool(ToolType type)
+    {
         currentToolType = type;
     }
 }
