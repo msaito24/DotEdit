4688b24754da6eaa780a0f16cf2df55cb601523f
Kohei Imatomi <kohei.imatomi@gmail.com>
2018-11-27 18:37:12 +0900

    CanvasUIを一部実装

--- Assets/DotEdit/Scripts/DotCanvasUI.cs
+++ Assets/DotEdit/Scripts/DotCanvasUI.cs
@@ -11,17 +11,42 @@ public class DotCanvasUI : MonoBehaviour
     public int sizeY;
     public Color[] colors;
 
+    public Color currentColor;
+
     // キャンバスの一部がクリックされたときに呼び出される
     public void OnClicked(Vector2 position)
     {
+        // クリックされた位置が不正な位置でないなら正しく currentColor 色を描画する.
+        Point point;
 
+        if(GetDrawPoint(position,null,out point))
+        {
+            DrawDot(point,currentColor);
+        }
     }
 
-    public void DrawDot(Point point, Color color) {
+    public void ChangeCurrentColor(Color color)
+    {
+        this.currentColor = color;
     }
 
-    public Point GetDrawPoint(Vector2 position, Camera camera) {
-        Point p = new Point(0, 0);
-        return p;
+    public void DrawDot(Point point,Color color)
+    {
+        int index = DotEditUtils.GetIndexFromPoint(point,sizeX);
+
+        if(0 <= index && index < images.Length)
+        {
+            images[index].color = color;
+        }
+        else
+        {
+            Debug.LogWarningFormat("不正な index: {0}",index);
+        }
+    }
+
+    public bool GetDrawPoint(Vector2 position,Camera camera,out Point point)
+    {
+        point = new Point(0,0);
+        return false;
     }
 }
